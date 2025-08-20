860a1be95797316d0dd4f98f01164a7f899c1c7f
kumar-kaushal-dev <kaushal1234kumar56789@gmail.com>
2025-08-20 23:14:39 +0530

    Update models.py

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # BlogPost model - database structure
 class BlogPost(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=101)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
 
